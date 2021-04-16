e74f2c81804a4be198d46011bbdd9ae30daecf45
bluethefoxofficial <47904806+bluethefoxofficial@users.noreply.github.com>
2021-04-16 03:07:32 +0100

    Added new buttons.

--- assets/js/map.js
+++ assets/js/map.js
@@ -12,15 +12,7 @@ var youicon = new icon({iconUrl: '/assets/img/you.png'});
 var atm = new icon({iconUrl: '/assets/img/atm.png'});
 var markers = L.markerClusterGroup();
 var dogemap = L.map('dogemap');
-if(getCookie("burntbiscuit") == "L"){
-L.tileLayer('http://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 20,
-    tileSize: 512,
-    zoomOffset: -1,
-    
-}).addTo(dogemap);
-}else if(getCookie("burntbiscuit") == "D"){
+
     L.tileLayer('http://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 20,
@@ -28,19 +20,24 @@ L.tileLayer('http://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}
         zoomOffset: -1,
         
     }).addTo(dogemap);
-}
+
 
 
 dogemap.locate({setView: true, maxZoom: 16});
 
+var umark;
 
 function onLocationFound(e) {
     var radius = e.accuracy;
-
-    markers.addLayer(L.marker(e.latlng,{
+    if(umark == null){
+    umark = L.marker(e.latlng,{
         icon: youicon
     }).addTo(dogemap)
-        .bindPopup("You are within " + radius + " meters from this point look around you might find some place that accept dogecoin near you."));
+        .bindPopup("You are within " + radius + " meters from this point look around you might find some place that accept dogecoin near you.");
+        dogemap.addLayer(umark);
+}else{
+    dogemap.addLayer(umark);
+}
 
    
 
@@ -51,17 +48,6 @@ dogemap.on('locationfound', onLocationFound);
 
 
 function developer(){
-    console.log("-----------------------------------");
-    console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-        console.log("-----------------------------------");
-    
     console.log("developer: @bluethefoxyt on twitter");
 }
 
@@ -74,45 +60,54 @@ $.get( "api/",{ function: "listalllocations"}, function( data ) {
     if(obj == null) return console.log("Obj is null check serverside API code");
     obj.forEach(el => {
         document.getElementById("loader").style.display = "block";
-        if(el.name == "CEX"){
-markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
-<img src="https://uk.webuy.com/images/logos/CeX_Logo_Rich_black_CMYK-01.png" width="30px" />
-<p style="color: lime;">VERIFIED</p>
-<p>ID: ${el.ID}</p>
-<p>Name: ${el.name}</p>
-<p>Address: ${el.address}</p>
-<a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
-<p>Description: ${el.description}</p>
-<a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
-`));
 
-}else if(el.name == "Csoftware"){
 
-    markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
-<img src="https://csoftware.cf/assets/img/logo.png" width="30px" />
-<p style="color: lime;">VERIFIED</p>
-<p>ID: ${el.ID}</p>
-<p>Name: ${el.name}</p>
-<p>Address: ${el.address}</p>
-<a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
-<p>Description: ${el.description}</p>
-<a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
-`));
+        switch(el.name){
+            //CEX lol marker
+            case "CEX":
+                markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
+                <img src="https://uk.webuy.com/images/logos/CeX_Logo_Rich_black_CMYK-01.png" width="30px" />
+                <p style="color: lime;">VERIFIED</p>
+                <p>ID: ${el.ID}</p>
+                <p>Name: ${el.name}</p>
+                <p>Address: ${el.address}</p>
+                <a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
+                <p>Description: ${el.description}</p>
+                <a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
+                `));
+            break;
+            //Csoftware marker
+            case "Csoftware":
 
-}else if(el.description.includes("ATM")){
+                markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
+                <img src="https://csoftware.cf/assets/img/logo.png" width="30px" />
+                <p style="color: lime;">VERIFIED</p>
+                <p>ID: ${el.ID}</p>
+                <p>Name: ${el.name}</p>
+                <p>Address: ${el.address}</p>
+                <a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
+                <p>Description: ${el.description}</p>
+                <a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
+                `));
 
-    markers.addLayer(L.marker([el.lat, el.long],{icon:atm},el.name).bindPopup(`
-<img src="/assets/img/atm.png" width="30px" />
-<p>A crypto ATM is a place where you can buy and sell dogecoin or other cryptocurrency's with a public machine.</p>
-<p>ID: ${el.ID}</p>
-<p>Name: ${el.name}</p>
-<p>Address: ${el.address}</p>
-<a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
-<p>Description: ${el.description}</p>
-<a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
-`));
-
-}else{
+            break;
+            
+            //the default location marker.
+            default:
+            switch(el.description){
+                case "ATM":
+                    markers.addLayer(L.marker([el.lat, el.long],{icon:atm},el.name).bindPopup(`
+                    <img src="/assets/img/atm.png" width="30px" />
+                    <p>A crypto ATM is a place where you can buy and sell dogecoin or other cryptocurrency's with a public machine.</p>
+                    <p>ID: ${el.ID}</p>
+                    <p>Name: ${el.name}</p>
+                    <p>Address: ${el.address}</p>
+                    <a class="n-o dogetext" href="http://${el.website}">Click here to access their website</a>
+                    <p>Description: ${el.description}</p>
+                    <a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
+                    `));
+                break;
+            default:
     markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
     <p>ID: ${el.ID}</p>
     <p>Name: ${el.name}</p>
@@ -121,7 +116,17 @@ markers.addLayer(L.marker([el.lat, el.long],el.name).bindPopup(`
     <p>Description: ${el.description}</p>
     <a href="report?id=${el.ID}">REPORT AS BEING FAKE</a>
     `));
-}
+    break;
+            }
+            break;
+        }
+
+
+
+
+
+
+
 });
 document.getElementById("loader").style.display = "none";
 });
@@ -131,11 +136,27 @@ dogemap.addLayer(markers);
 var follow = false;
 function locate() {
     if(follow == true){
+    dogemap.removeLayer(umark);
     dogemap.locate({setView: true, maxZoom: 16});
     }
   }
 
   // call locate every 3 seconds... forever
-  setInterval(locate, 3000);
+  setInterval(locate, 5000);
 
 //L.marker([51.505, -0.09],{title: "example"}).addTo(dogemap).bindPopup("Im a weird thing."); Example for devs.
+
+
+
+var button = new L.Control.Button('follow');
+button.addTo(dogemap);
+button.on('click', function () {
+    switch(follow){
+        case true:
+        follow = false;
+        break;
+        case false:
+        follow = true;
+        break;
+    }
+});
--- internal/configure.php
+++ internal/configure.php
@@ -5,4 +5,5 @@ $DBHOST = "localhost"; //host usually localhost unless you're weird.
 $DBUSERNAME = "..."; //database username
 $DBPASSWORD = "..."; //database password
 $DATABASE = "..."; //the bloody database
+
 ?>
